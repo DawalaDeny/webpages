6d3b30cab75793739ae00b653acbcef1f42a84e8
DawalaDeny <shabouevdeny@gmail.com>
2023-09-11 22:24:38 +0200

    aaa

--- projects/AvatarMemoryGame2/backend/controllers/scores.js
+++ projects/AvatarMemoryGame2/backend/controllers/scores.js
@@ -1,12 +1,21 @@
 //error
-
+const mongoose = require('mongoose')
 const { StatusCodes, PERMANENT_REDIRECT } = require('http-status-codes')
 const Score = require('../models/score')
 
 const getScores =async (req,res) =>{
     try {
-        const scores = await Score.find();
-        res.status(StatusCodes.OK).json({ scores })
+        let scores = await Score.find();
+        let filteredScores = [];
+        scores.forEach(score => {
+            filteredScores.push({
+                name: score.name, 
+                score: score.score });
+        });
+        console.log(filteredScores);
+       
+        filteredScores.sort((a, b) => a.score - b.score);
+        res.status(StatusCodes.OK).json({ filteredScores })
     } catch (error) {
         res.status(StatusCodes.BAD_REQUEST).json({ err:error.message })
     }
