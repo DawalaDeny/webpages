2ed80a14915e7fb01037ad1f80a4f8f8c02e7bcb
DawalaDeny <shabouevdeny@gmail.com>
2023-11-18 20:10:29 +0100

    cloud backend

---
+++ projects/Cloud Infrastructure/backend/app.js
@@ -0,0 +1,42 @@
+require('express-async-errors');
+require('dotenv').config()
+
+const helmet = require('helmet');
+const express = require ("express");
+const app = express();
+const port= process.env.PORT || 80;
+
+const connectDB = require('./db/dbConnect')
+const scores = require("./routes/scores")
+const xss = require('xss-clean');
+const errorHandler = require('./middleware/errorHandlerMiddleware')
+
+//middleware
+app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'], 
+        
+      },
+    })
+  );
+app.use(xss())
+app.use(express.json())
+
+
+app.use('/api/v1/scores', scores)
+
+app.use(errorHandler);
+
+const start = async () =>{
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`server is listening to port: ${port}...`));
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
---
+++ projects/Cloud Infrastructure/frontend/script.js
@@ -0,0 +1,19 @@
+let page = 1;
+let max = 1;
+
+const start =  () => {
+   
+        const main = document.getElementsByClassName("main")[0];
+        let helloworld = document.createElement("p")
+        helloworld.innerHTML = "hello world!!!"
+        main.appendChild(helloworld);
+    
+      
+   
+};
+
+const fetch = async () =>{
+   
+}
+
+document.addEventListener('DOMContentLoaded', start);
